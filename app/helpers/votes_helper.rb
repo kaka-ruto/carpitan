33879066421857a2f1d81379ddaeb80d5fa5d98f
Kaka Ruto <kaka@collinsruto.com>
2024-06-20 15:56:31 +0100

    Change badge colors

--- app/helpers/votes_helper.rb
+++ app/helpers/votes_helper.rb
@@ -2,9 +2,9 @@ module VotesHelper
   def vote_choice(vote)
     case vote.choice
     when "yes"
-      tag.span "Yes", class: "badge bg-success"
+      tag.span "Yes", class: "badge bg-danger"
     when "no"
-      tag.span "No", class: "badge bg-danger"
+      tag.span "No", class: "badge bg-success"
     when "abstain"
       tag.span "Abstain", class: "badge bg-warning"
     end
